e2f2fc6af62649254c22ee5477388996f409d595
areenas <116331213+areenas@users.noreply.github.com>
2022-10-21 10:21:28 +0530

    Add files via upload

---
+++ program 4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+temp=num1
+num1=num2
+num2=temp
+print("num1",num1)
+print("num2",num2)
+
+
+# In[ ]:
+
+
+
+
