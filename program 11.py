5c7a7cc8e4698cfcf800800d2596eb70554040db
areenas <116331213+areenas@users.noreply.github.com>
2022-11-04 11:42:51 +0530

    Add files via upload

---
+++ program 11.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+i=int(input("Enter the number:"))
+print("Multiplication of ",i,"is")
+for n in range (1,11):
+    sum=n*i
+    print(i,"*",n,"=",sum)
+
+
+# In[ ]:
+
+
+
+
