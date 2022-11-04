980831efe05b8d7ac52eec77cdb371db82b97507
areenas <116331213+areenas@users.noreply.github.com>
2022-11-04 11:35:19 +0530

    Add files via upload

---
+++ program9.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a= int(input("Enter the number:"))
+b= int(input("Enter the number:"))
+op=input("Enter the operator: +,-,*,/,%,^ ")
+if(op=='+'):
+    print("addition",a+b)
+elif(op=='-'):
+    print("substraction",a-b)
+elif(op=='*'):
+    print("multiplication",a*b)
+elif(op=='/'):
+    print("Division",a/b)
+elif(op=='%'):
+    print("modula",a%b)
+elif(op=='^'):
+    print("power",a^b)
+else:
+    print("Ivalid choice")
+       
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
