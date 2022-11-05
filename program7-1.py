a51941a563722a57fd34542734fef682997f3c21
areenas <116331213+areenas@users.noreply.github.com>
2022-11-05 10:03:39 +0530

    Add files via upload

---
+++ program7-1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a= int(input(Enter the number:))
+b= int(input(Enter the number:))
+op=int(input("Enter the operator: +,-,*,/,%,^")
+if(op== +)
+       print("addition",a+b)
+if(op== -)
+       print("substraction",a-b)
+if(op== *)
+       print("multiplication",a*b)
+if(op== /)
+       print("Division",a/b)
+if(op== %)
+       print("modula",a%b)
+if(op== ^)
+       print("power",a^b)
+       
+
