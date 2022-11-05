42efa1802d59861e679b9541feae22a2b4bed356
areenas <116331213+areenas@users.noreply.github.com>
2022-11-05 10:02:46 +0530

    Add files via upload

---
+++ program 6.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import cmath
+a=int(input("enter the number : "))
+b=int(input("enter the number : "))
+c=int(input("enter the number : "))
+d=(b**2)-(4*a*c)
+sol1=(-b+cmath.sqrt(d))/2*a
+sol2=(-b-cmath.sqrt(d))/2*a
+print("the solutions are : {}and{}".format(sol1,sol2))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
